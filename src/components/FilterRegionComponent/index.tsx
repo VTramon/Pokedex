d1de9a26f2a1288e9a716db660782cae45557ff8
VTramon <vitorr.ooliveira@gmail.com>
2022-03-04 09:55:12 -0300

    styled the filter components

--- src/components/FilterRegionComponent/index.tsx
+++ src/components/FilterRegionComponent/index.tsx
@@ -11,35 +11,33 @@ import {
 const FilterRegionComponent = () => {
   const dispatch = useDispatch()
   return (
-    <ul className={styles.filter_region_list}>
-      <li>
-        <span>
-          <button onClick={() => dispatch(toKanto())} value="Kanto">
-            Kanto
-          </button>
-        </span>
-        <span>
-          <button onClick={() => dispatch(toJohto())} value="Johto">
-            Johto
-          </button>
-        </span>
-        <span>
-          <button onClick={() => dispatch(toHoenn())} value="Hoenn">
-            Hoenn
-          </button>
-        </span>
-        <span>
-          <button onClick={() => dispatch(toSinnoh())} value="Sinnoh">
-            Sinnoh
-          </button>
-        </span>
-        <span>
-          <button onClick={() => dispatch(toUnova())} value="Unova">
-            Unova
-          </button>
-        </span>
-      </li>
-    </ul>
+    <div className={styles.filter_region_list}>
+      <span>
+        <button onClick={() => dispatch(toKanto())} value="Kanto">
+          Kanto
+        </button>
+      </span>
+      <span>
+        <button onClick={() => dispatch(toJohto())} value="Johto">
+          Johto
+        </button>
+      </span>
+      <span>
+        <button onClick={() => dispatch(toHoenn())} value="Hoenn">
+          Hoenn
+        </button>
+      </span>
+      <span>
+        <button onClick={() => dispatch(toSinnoh())} value="Sinnoh">
+          Sinnoh
+        </button>
+      </span>
+      <span>
+        <button onClick={() => dispatch(toUnova())} value="Unova">
+          Unova
+        </button>
+      </span>
+    </div>
   )
 }
 
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from '../Filter'
 import PokemonContainer from '../PokemonContainer'
 import styles from './Layout.module.scss'
 
 const Layout = () => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+  }, [])
   return (
-    <main className={styles.layout}>
-      <Filter />
-      <PokemonContainer />
-    </main>
+    <>
+      {loading ? (
+        <main className={styles.layout}>
+          <Filter />
+          <PokemonContainer />
+        </main>
+      ) : (
+        'loading'
+      )}
+    </>
   )
 }
 
